a9d23f697f46e3360d8e250bbd7d2fdf0ab1383c
Scott Westover <scottwestover2006@gmail.com>
2023-03-20 13:15:04 -0400

    storing changes
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- games/tic-tac-toe/src/main.ts
+++ games/tic-tac-toe/src/main.ts
@@ -1,16 +1,48 @@
 import Phaser from 'phaser';
 
+const SPRITE_ASSET_KEY = 'SPRITE_ASSET_KEY';
+
 class Game extends Phaser.Scene {
+  #playerTurnText!: Phaser.GameObjects.Text;
+
   constructor() {
     super({ key: 'Game' });
   }
 
   preload(): void {
-    // load in data
+    this.load.spritesheet(SPRITE_ASSET_KEY, 'assets/images/blocks.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
   }
 
   create(): void {
-    // Create game objects
+    const t = this.add
+      .text(240, 50, 'Tic-Tac-Toe', {
+        color: 'purple',
+        fontFamily: 'Verdana',
+        fontSize: '42px',
+      })
+      .setOrigin(0.5);
+    console.log(t.getBounds());
+
+    // add text object about current players turn
+    const d = (this.#playerTurnText = this.add
+      .text(240, 600, 'X turn', {
+        color: 'black',
+        fontFamily: 'Verdana',
+        fontSize: '22px',
+      })
+      .setOrigin(0.5));
+    console.log(d.getBounds());
+
+    // draw lines between game spaces
+    const graphics = this.add.graphics();
+    graphics.lineStyle(12, 0x3e3e3e);
+    graphics.lineBetween(170, 120, 170, 540);
+    graphics.lineBetween(314, 120, 314, 540);
+    graphics.lineBetween(30, 258, 450, 258);
+    graphics.lineBetween(30, 402, 450, 402);
   }
 }
 
