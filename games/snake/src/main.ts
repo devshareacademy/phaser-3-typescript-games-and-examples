937ddd1ad09c6caf793ccc2b9b4e80d65aaad99a
Scott Westover <scottwestover2006@gmail.com>
2024-02-04 20:37:07 -0500

    added snake game prototype
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

---
+++ games/snake/src/main.ts
@@ -0,0 +1,118 @@
+import * as Phaser from 'phaser';
+import { DIRECTION, Snake } from './snake';
+
+const SPRITE_ASSET_KEY = 'SPRITE_ASSET_KEY';
+const SPRITE_SIZE = 16;
+const SCALE_SIZE = 2;
+
+class Game extends Phaser.Scene {
+  #snake!: Snake;
+  #snakeGameObjectPool!: Phaser.GameObjects.Group;
+  #apple!: Phaser.GameObjects.Image;
+  #cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+  #movePlayerTimer!: number;
+  #speed!: number;
+
+  constructor() {
+    super({ key: 'Game' });
+  }
+
+  public init(): void {
+    this.#snake = new Snake();
+    this.#speed = 200;
+  }
+
+  public preload(): void {
+    this.load.spritesheet(SPRITE_ASSET_KEY, 'assets/images/snake.png', {
+      frameWidth: SPRITE_SIZE,
+      frameHeight: SPRITE_SIZE,
+    });
+  }
+
+  public create(): void {
+    this.#apple = this.add
+      .image(
+        this.#snake.apple.position.x * SPRITE_SIZE * SCALE_SIZE,
+        this.#snake.apple.position.y * SPRITE_SIZE * SCALE_SIZE,
+        SPRITE_ASSET_KEY,
+        0,
+      )
+      .setOrigin(0)
+      .setScale(SCALE_SIZE);
+
+    this.#snakeGameObjectPool = this.add.group();
+    this.#snake.player.cells.forEach((cell) => {
+      const obj = this.#snakeGameObjectPool.get(
+        cell.x * SPRITE_SIZE * SCALE_SIZE,
+        cell.y * SPRITE_SIZE * SCALE_SIZE,
+        SPRITE_ASSET_KEY,
+        1,
+        true,
+      ) as Phaser.GameObjects.Image;
+      obj.setScale(SCALE_SIZE).setOrigin(0);
+    });
+
+    this.#cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  public update(time: number): void {
+    if (this.#snake.isGameOver) {
+      return;
+    }
+
+    if (this.#cursors.left.isDown && this.#snake.player.direction !== DIRECTION.RIGHT) {
+      this.#snake.playerDirection = DIRECTION.LEFT;
+    } else if (this.#cursors.right.isDown && this.#snake.player.direction !== DIRECTION.LEFT) {
+      this.#snake.playerDirection = DIRECTION.RIGHT;
+    } else if (this.#cursors.up.isDown && this.#snake.player.direction !== DIRECTION.DOWN) {
+      this.#snake.playerDirection = DIRECTION.UP;
+    } else if (this.#cursors.down.isDown && this.#snake.player.direction !== DIRECTION.UP) {
+      this.#snake.playerDirection = DIRECTION.DOWN;
+    }
+
+    if (time < this.#movePlayerTimer) {
+      return;
+    }
+
+    this.#snake.update();
+
+    this.#snakeGameObjectPool.getChildren().forEach((child) => {
+      child.setActive(false);
+    });
+
+    this.#snake.player.cells.forEach((cell) => {
+      const obj = this.#snakeGameObjectPool.get(
+        cell.x * SPRITE_SIZE * SCALE_SIZE,
+        cell.y * SPRITE_SIZE * SCALE_SIZE,
+        SPRITE_ASSET_KEY,
+        1,
+        true,
+      ) as Phaser.GameObjects.Image;
+      obj.setScale(SCALE_SIZE).setOrigin(0).setActive(true);
+    });
+    this.#apple.setPosition(
+      this.#snake.apple.position.x * SPRITE_SIZE * SCALE_SIZE,
+      this.#snake.apple.position.y * SPRITE_SIZE * SCALE_SIZE,
+    );
+
+    if (this.#snake.isGameOver) {
+      console.log('game over');
+    }
+
+    this.#movePlayerTimer = time + this.#speed;
+  }
+}
+
+const gameConfig: Phaser.Types.Core.GameConfig = {
+  type: Phaser.CANVAS,
+  pixelArt: true,
+  scale: {
+    parent: 'game-container',
+    width: 640,
+    height: 640,
+  },
+  backgroundColor: '#5c5b5b',
+  scene: [Game],
+};
+
+const game = new Phaser.Game(gameConfig);
---
+++ games/snake/src/snake.ts
@@ -0,0 +1,152 @@
+export type Direction = keyof typeof DIRECTION;
+
+export const DIRECTION = {
+  UP: 'UP',
+  DOWN: 'DOWN',
+  LEFT: 'LEFT',
+  RIGHT: 'RIGHT',
+} as const;
+
+type Coordinate = {
+  x: number;
+  y: number;
+};
+
+type Player = {
+  position: Coordinate;
+  length: number;
+  cells: Coordinate[];
+  direction: Direction;
+};
+
+type Apple = {
+  position: Coordinate;
+};
+
+function randomNumberBetween(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export class Snake {
+  #numberOfRows: number;
+  #numberOfCols: number;
+  #player: Player;
+  #apple: Apple;
+  #gameOver: boolean;
+
+  constructor() {
+    this.#numberOfRows = 20;
+    this.#numberOfCols = 20;
+    this.#player = {
+      cells: [],
+      length: 4,
+      position: {
+        x: 10,
+        y: 10,
+      },
+      direction: DIRECTION.RIGHT,
+    };
+    for (let i = 0; i < this.#player.length; i += 1) {
+      this.#player.cells.push({
+        x: this.#player.position.x - i,
+        y: this.#player.position.y,
+      });
+    }
+    this.#apple = {
+      position: {
+        x: 0,
+        y: 0,
+      },
+    };
+    this.#spawnApple();
+    this.#gameOver = false;
+  }
+
+  get player(): Player {
+    return this.#player;
+  }
+
+  get apple(): Apple {
+    return this.#apple;
+  }
+
+  get isGameOver(): boolean {
+    return this.#gameOver;
+  }
+
+  set playerDirection(direction: Direction) {
+    this.#player.direction = direction;
+  }
+
+  public update(): void {
+    this.#movePlayer();
+    this.#checkForCollisions();
+  }
+
+  #movePlayer(): void {
+    // move the snake object
+    switch (this.#player.direction) {
+      case DIRECTION.DOWN:
+        this.#player.position.y += 1;
+        break;
+      case DIRECTION.UP:
+        this.#player.position.y -= 1;
+        break;
+      case DIRECTION.LEFT:
+        this.#player.position.x -= 1;
+        break;
+      case DIRECTION.RIGHT:
+        this.#player.position.x += 1;
+        break;
+    }
+
+    // wrap snake position horizontally on the edge of the screen
+    if (this.#player.position.x < 0) {
+      this.#player.position.x = this.#numberOfCols - 1;
+    } else if (this.#player.position.x >= this.#numberOfCols) {
+      this.#player.position.x = 0;
+    }
+
+    // wrap snake position vertically on the edge of the screen
+    if (this.#player.position.y < 0) {
+      this.#player.position.y = this.#numberOfRows - 1;
+    } else if (this.#player.position.y >= this.#numberOfRows) {
+      this.#player.position.y = 0;
+    }
+
+    // update the snake cells, first cell is the head of the snake
+    this.#player.cells.unshift({
+      x: this.#player.position.x,
+      y: this.#player.position.y,
+    });
+
+    // remove the last cell since we added one to the beginning of our array
+    if (this.#player.cells.length > this.#player.length) {
+      this.#player.cells.pop();
+    }
+  }
+
+  #checkForCollisions(): void {
+    // check for collision with apple
+    if (this.#player.position.x === this.#apple.position.x && this.#player.position.y === this.#apple.position.y) {
+      // add to the snake body
+      this.#player.length += 1;
+
+      // respawn the apple
+      this.#spawnApple();
+    }
+
+    // check to see if the snake collided with itself
+    for (let i = 1; i < this.#player.cells.length; i += 1) {
+      if (this.#player.position.x === this.#player.cells[i].x && this.#player.position.y === this.#player.cells[i].y) {
+        this.#gameOver = true;
+        break;
+      }
+    }
+  }
+
+  #spawnApple(): void {
+    this.#apple.position.x = randomNumberBetween(0, this.#numberOfCols - 1);
+    this.#apple.position.y = randomNumberBetween(0, this.#numberOfRows - 1);
+  }
+}
