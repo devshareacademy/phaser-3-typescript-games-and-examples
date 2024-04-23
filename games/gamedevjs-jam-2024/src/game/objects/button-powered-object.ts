f9d474f43738dd3826e501a48b4e63322c1cb082
Scott Westover <scottwestover2006@gmail.com>
2024-04-22 22:15:44 -0400

    cleaned up code after migration
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- games/gamedevjs-jam-2024/src/game/objects/button-powered-object.ts
+++
@@ -1,4 +0,0 @@
-export interface ButtonPoweredObject {
-  powerLevelChanged(powerLevel: number): void;
-  setInitialPowerLevel(powerLevel: number): void;
-}
--- games/gamedevjs-jam-2024/src/game/scenes/scene-keys.ts
+++
@@ -1,7 +0,0 @@
-export const SceneKeys = {
-  BootScene: 'BootScene',
-  GameScene: 'GameScene',
-  PreloadScene: 'PreloadScene',
-  TitleScene: 'TitleScene',
-  CreditsScene: 'CreditsScene',
-} as const;
