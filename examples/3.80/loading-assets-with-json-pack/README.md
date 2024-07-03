f7e33ba46d7a406f8fa0d6cd4229bcfa3583728e
Scott Westover <scottwestover2006@gmail.com>
2024-07-02 22:25:22 -0400

    added example for loading assets via json file
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

---
+++ examples/3.80/loading-assets-with-json-pack/README.md
@@ -0,0 +1,54 @@
+# Phaser 3 TypeScript - Loading Assets via Pack JSON
+
+Contains example code of how you can use the built in [Phaser 3 Pack Loader](https://newdocs.phaser.io/docs/3.80.0/focus/Phaser.Loader.LoaderPlugin-pack) to load your various asset files from one central JSON file. This can help you keep your code and config isolated.
+
+Contains example code of how you can use the built in Phaser 3 Arcade Physics Engine to create a conveyor belt like game object that will move objects automatically, and modify their speed when the objects are moving.
+
+You can see a live demo of the example here: [Phaser 3 Asset Loading via Pack Loader Demo](https://devshareacademy.github.io/phaser-3-typescript-games-and-examples/examples/arcade-physics-conveyor-belt/index.html)
+
+![Example](./docs/example.png?raw=true)
+
+## Local Development
+
+### Requirements
+
+[Node.js](https://nodejs.org) and [pNPm](https://pnpm.io/) are required to install dependencies and run scripts via `pnpm`.
+
+[Vite](https://vitejs.dev/) is required to bundle and serve the web application. This is included as part of the projects dev dependencies.
+
+### Available Commands
+
+| Command | Description |
+|---------|-------------|
+| `pnpm install --frozen-lockfile` | Install project dependencies |
+| `pnpm start` | Build project and open web server running project |
+| `pnpm build` | Builds code bundle for production |
+| `pnpm lint` | Uses ESLint to lint code |
+
+### Writing Code
+
+After cloning the repo, run `pnpm install --frozen-lockfile` from your project directory. Then, you can start the local development
+server by running `pnpm start`.
+
+After starting the development server with `pnpm start`, you can edit any files in the `src` folder
+and parcel will automatically recompile and reload your server (available at `http://localhost:8080`
+by default).
+
+### Deploying Code
+
+After you run the `pnpm build` command, your code will be built into a single bundle located at
+`dist/*` along with any other assets you project depended.
+
+If you put the contents of the `dist` folder in a publicly-accessible location (say something like `http://myserver.com`),
+you should be able to open `http://myserver.com/index.html` and play your game.
+
+### Static Assets
+
+Any static assets like images or audio files should be placed in the `public` folder. It'll then be served at `http://localhost:8080/path-to-file-your-file/file-name.file-type`.
+
+## Credits
+
+List of assets from some great artists used in this demo:
+
+* [xdeviruchi Sound Cloud](https://soundcloud.com/xdeviruchi)
+* [Small 8-direction Characters](https://axulart.itch.io/small-8-direction-characters)
---
+++ examples/3.80/loading-assets-with-json-pack/src/main.ts
@@ -0,0 +1,90 @@
+import * as Phaser from 'phaser';
+
+export class Game extends Phaser.Scene {
+  constructor() {
+    super({ key: 'Game' });
+  }
+
+  public preload(): void {
+    this.load.setPath('assets/images/axulart/character');
+    this.load.spritesheet('player', 'custom.png', { frameWidth: 64, frameHeight: 88 });
+
+    this.load.setPath('assets/images/monster-tamer/battle-backgrounds');
+    this.load.image('forest', 'forest-background.png');
+
+    this.load.setPath('assets/images/monster-tamer/monsters');
+    this.load.image('carnodusk', 'carnodusk.png');
+    this.load.image('iguanignite', 'iguanignite.png');
+    this.load.image('aquavalor', 'aquavalor.png');
+    this.load.image('frostsaber', 'frostsaber.png');
+    this.load.image('ignivolt', 'ignivolt.png');
+    this.load.image('parazoid', 'parazoid.png');
+    this.load.image('jivy', 'jivy.png');
+
+    this.load.setPath('assets/data');
+    this.load.json('item_data', 'items.json');
+
+    this.load.setPath('assets/audio/xDeviruchi');
+    this.load.audio('title_theme', 'Title-Theme.wav');
+
+    // duplicated for example
+    // this.load.setPath('assets/images/axulart/character');
+    // this.load.spritesheet('player', 'custom.png', { frameWidth: 64, frameHeight: 88 });
+
+    // this.load.setPath('assets/images/monster-tamer/battle-backgrounds');
+    // this.load.image('forest', 'forest-background.png');
+
+    // this.load.setPath('assets/images/monster-tamer/monsters');
+    // this.load.image('carnodusk', 'carnodusk.png');
+    // this.load.image('iguanignite', 'iguanignite.png');
+    // this.load.image('aquavalor', 'aquavalor.png');
+    // this.load.image('frostsaber', 'frostsaber.png');
+    // this.load.image('ignivolt', 'ignivolt.png');
+    // this.load.image('parazoid', 'parazoid.png');
+    // this.load.image('jivy', 'jivy.png');
+
+    // this.load.setPath('assets/data');
+    // this.load.json('item_data', 'items.json');
+
+    // this.load.setPath('assets/audio/xDeviruchi');
+    // this.load.audio('title_theme', 'Title-Theme.wav');
+
+    // asset pack example
+    // this.load.pack('asset_pack', 'assets/data/assets.json');
+  }
+
+  public create(): void {
+    this.add.image(0, 0, 'forest').setOrigin(0);
+    this.add.image(30, 450, 'ignivolt').setOrigin(0).setScale(0.5);
+    this.add.image(540, 450, 'carnodusk').setOrigin(0).setScale(0.5);
+    this.add.image(420, 450, 'iguanignite').setOrigin(0).setScale(0.5);
+    this.add.image(290, 450, 'aquavalor').setOrigin(0).setScale(0.5);
+    this.add.image(160, 450, 'frostsaber').setOrigin(0).setScale(0.5);
+    this.add.image(680, 450, 'parazoid').setOrigin(0).setScale(0.5);
+    this.add.image(810, 450, 'jivy').setOrigin(0).setScale(0.5);
+    this.add.image(0, 0, 'player', 0).setOrigin(0);
+    this.add.image(0, 100, 'player', 1).setOrigin(0);
+    this.add.image(0, 200, 'player', 2).setOrigin(0);
+    this.add.image(0, 300, 'player', 3).setOrigin(0);
+    this.add.image(100, 0, 'player', 4).setOrigin(0);
+    this.add.image(200, 0, 'player', 5).setOrigin(0);
+    this.add.image(300, 0, 'player', 6).setOrigin(0);
+    this.sound.play('title_theme', {
+      loop: true,
+      volume: 0.1,
+    });
+  }
+}
+
+const gameConfig: Phaser.Types.Core.GameConfig = {
+  type: Phaser.WEBGL,
+  pixelArt: true,
+  scale: {
+    parent: 'game-container',
+    width: 1024,
+    height: 576,
+  },
+  scene: [Game],
+};
+
+new Phaser.Game(gameConfig);
