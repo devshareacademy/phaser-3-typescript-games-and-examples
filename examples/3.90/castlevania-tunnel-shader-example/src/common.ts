4ad28527857afe3673751cd03fbeb877d963ce35
Scott Westover <scottwestover2006@gmail.com>
2025-08-27 09:57:48 -0400

    created shader for tunnel effect from castlevania
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

---
+++ examples/3.90/castlevania-tunnel-shader-example/src/common.ts
@@ -0,0 +1,9 @@
+export const ASSET_KEYS = Object.freeze({
+  GRID: 'GRID',
+  TUNNEL: 'TUNNEL',
+});
+
+export const SCENE_KEYS = Object.freeze({
+  LOADING_SCENE: 'LOADING_SCENE',
+  GAME_SCENE: 'GAME_SCENE',
+});
